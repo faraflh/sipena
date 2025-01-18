b6fba0c0633130e79a11d32fbae5aa8474e22bd7
Dev <dev@riau.go.id>
2025-01-18 13:57:21 +0700

    card login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -63,12 +63,12 @@
                 </div>
                 <div class="container" data-aos="fade-up" data-aos-delay="100">
                     <div class="form-container row g-2">
-                        <div class="col-lg-12" style="width: 500px; height: 330px;">
+                        <div class="col-lg-12" style="width: 500px; height: 320px;">
                             <div class="info-item d-flex flex-column justify-content-center aos-init aos-animate equal-height" data-aos="fade-up" data-aos-delay="200">
                                 <h4>Sistem Informasi Pengelolaan Administrasi</h4>
                             </div>
                         </div>
-                        <div class="login col-lg-12" style="width: 500px; height:330px;">
+                        <div class="login col-lg-12" style="width: 500px; height:320px;">
                             <div class="info-item d-flex flex-column justify-content-center equal-height">
                                 @if(session('error'))
                                     <div class="alert alert-danger">
